414f051cd8f19c40956d909f1d82ca0d3e526559
Ivan Trepakov <liontiger23@gmail.com>
2024-06-09 15:40:01 +0700

    Add basic routing

--- src/Server.hs
+++ src/Server.hs
@@ -7,14 +7,33 @@ import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 
 app :: Application
-app _ respond = do
-    putStrLn "I've done some IO here"
-    respond $ responseLBS
-        status200
-        [("Content-Type", "text/plain")]
-        "Hello, Web!"
+app request respond = do
+  respond $ case rawPathInfo request of
+    "/" -> index
+    "/test/" -> text
+    _ -> notFound
+
+text :: Response
+text = responseLBS
+    status200
+    [("Content-Type", "text/plain")]
+    "Hello, Web!"
+
+index :: Response
+index = responseFile
+  status200
+  [("Content-Type", "text/html")]
+  "index.html"
+  Nothing
+
+notFound :: Response
+notFound = responseFile
+  status200
+  [("Content-Type", "text/html")]
+  "404.html"
+  Nothing
 
 main :: IO ()
 main = do
-    putStrLn $ "http://localhost:8080/"
-    run 8080 app
+  putStrLn "http://localhost:8080/"
+  run 8080 app
